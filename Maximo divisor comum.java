fd236bbb9c06e9b3fd8ca2858c79fe821e96323e
Agripino2407 <lagripino07@gmail.com>
2025-04-01 18:37:37 -0300

    Create Maximo divisor comum.java

---
+++ Maximo divisor comum.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static int mdc(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mdc(48, 18));  
+    }
+}
